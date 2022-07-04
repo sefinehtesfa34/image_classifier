e9c37f99b13435dec31c57484284f2d8fbd27c70
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-04 11:03:21 +0300

    download the flower dataset

--- classifier.py
+++ classifier.py
@@ -7,3 +7,8 @@ import tensorflow_datasets as tfds
 import pathlib
 import matplotlib.pyplot as plt
 
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(origin=dataset_url,
+                                   fname='flower_photos',
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
